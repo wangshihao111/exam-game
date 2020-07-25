7434db67aa014680f2d422e17607768a0a40d03a
shihao wang <shihao.wang@hand-china.com>
2020-07-25 20:13:15 +0800

    接口实现

--- server/routes/index.route.ts
+++ server/routes/index.route.ts
@@ -6,14 +6,32 @@ export default class IndexController extends RouteController {
   constructor(router: Router) {
     super(router)
   }
+
   public init(): void {
-    // this.router.get('/', this.index.bind(this));
-    this.router.get('/users', this.getUsers.bind(this))
+    this.router.get('/', this.index.bind(this));
+    this.router.get('/users', this.getUsers.bind(this));
+    this.router.get('/data', this.getData.bind(this));
+    this.router.post('/data', this.setData.bind(this))
   }
   private async index(ctx: Context, next: Next) {
     ctx.body = 'hello, koajs.'
   }
   private async getUsers(ctx: Context, next: Next) {
     ctx.body = await this.db.get('users');
+  }
+  private async getData(ctx: Context, next: Next) {
+    const id = ctx.request.query.id;
+    ctx.body = (await this.db.get('users') || {})[id] || {};
+  }
+  private async setData(ctx: Context, next: Next) {
+    const current = await this.db.get('users') || {};
+    const body = ctx.request.body;
+    console.log(body);
+    const {id, data} = body;
+    current[id] = data;
+    ctx.body = {
+      code: 0,
+      msg: '更新成功。'
+    }
   }
 }
