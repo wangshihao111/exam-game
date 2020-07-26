ead0ed576d7a2e2607a8449c2b034d6b571a05a8
shihao wang <shihao.wang@hand-china.com>
2020-07-26 10:56:05 +0800

    服务器静态资源和压缩

--- server/app.ts
+++ server/app.ts
@@ -4,15 +4,19 @@ import { createRootContainer } from "what-di";
 import initRouter from "./routes";
 import DBUtils from "./utils/db.utils";
 import bodyParser from 'koa-bodyparser';
+import koaStatic from 'koa-static';
+import {resolve} from "path";
+import compress from 'koa-compress'
 
 createRootContainer({
   providers: [{ provide: "db", useValue: new DBUtils() }],
 });
 
 const app = new Koa();
-
+app.use(compress())
 app.use(cors());
 app.use(bodyParser())
+app.use(koaStatic(resolve(__dirname, '../dist')));
 
 initRouter(app);
 
